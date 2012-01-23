4b8e943a9ac733d54bb07fc2fc7fb807f480dac0
buty <buty4649@gmail.com>
2012-01-24 02:33:20 +0900

    WikiName実装(ただし、バグあり)

--- redcarpet/parser.rb
+++ redcarpet/parser.rb
@@ -16,6 +16,35 @@ class HikiHTMLRender < Redcarpet::Render::HTML
       level = header_level + @top_level
       "<h#{level}>#{text}</h#{level}>"
   end
+
+  WIKI_LINK_RE = /\[\[.+?\]\]/
+  WIKI_NAME_RE = /\b(?:[A-Z]+[a-z\d]+){2,}\b/
+  def paragraph(txt)
+      re = /(#{WIKI_LINK_RE})|(#{WIKI_NAME_RE})/xo
+      buf = ""
+      str = txt
+      while m = re.match(str)
+      	buf << m.pre_match
+	str = m.post_match
+
+	wiki_link, wiki_name = m[1,2]
+	if wiki_link
+	   buf << compile_wiki_link(wiki_link[2...-2])
+	elsif wiki_name
+	   buf << %Q|<a href="#{wiki_name}">#{wiki_name}</a>|
+	end
+      end
+      buf << str
+      %Q|<p>#{buf}</p>\n|
+  end
+
+  def compile_wiki_link(link)
+     if m = /\A(?>[^|\\]+|\\.)*\|/.match(link)
+       %Q|<a href="#{m.post_match}">#{m[0].chop}</a>|
+     else
+       %Q|<a href="#{link}">#{link}</a>|
+     end
+  end
 end
 
 module Hiki
@@ -47,7 +76,7 @@ module Hiki
 
     def parse( s, top_level = 2 )
       render = HikiHTMLRender.new(top_level)
-      markdown = Redcarpet::Markdown.new(render,:autolink => true, :space_after_headers => true,:fenced_code_blocks => true, :tables => true, :strikethrough => true, :superscript => true)
+      markdown = Redcarpet::Markdown.new(render,:autolink => false, :space_after_headers => true,:fenced_code_blocks => true, :tables => true, :strikethrough => true, :superscript => true)
       markdown.render(s)
     end
   end
